717111f00403ef389fd99dcb1c507f62422d0daa
Jerzy Kiryczuk <jurkirk@gmail.com>
2019-04-15 11:14:07 +0200

    Button with modal that gets name and send to the backend

--- white-test-app-frontend/src/app/app-routing.module.ts
+++ white-test-app-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {TestTemplatesComponent} from './test-templates/test-templates.component';
+import {PositionsComponent} from './positions/positions.component';
 
 
 const routes: Routes = [
   {path: '', component: AppComponent},
   {path: 'test-template', component: TestTemplatesComponent},
+  {path: 'positions', component: PositionsComponent}
 
 ];
 
