09a63f35cbdd76085848e32685e9b964921017c3
Bartlomiej Ciach <203860@edu.p.lodz.pl>
2019-05-13 19:09:51 +0200

    MAS19WH-57 Code refactor and clean

--- white-test-app-frontend/src/app/all-test-results/all-test-results.component.spec.ts
+++ white-test-app-frontend/src/app/all-test-results/all-test-results.component.spec.ts
@@ -1,6 +1,6 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AllTestResultsComponent } from './all-test-results.component';
+import {AllTestResultsComponent} from './all-test-results.component';
 
 describe('AllTestResultsComponent', () => {
   let component: AllTestResultsComponent;
