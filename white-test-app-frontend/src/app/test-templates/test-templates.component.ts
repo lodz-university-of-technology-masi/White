5c921acd643720b255de94dfa00c6451846ee622
marthastamp <marta.stempel@gmail.com>
2019-05-23 00:57:09 +0200

    merge conflict

--- white-test-app-frontend/src/app/test-templates/test-templates.component.ts
+++ white-test-app-frontend/src/app/test-templates/test-templates.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnInit} from '@angular/core';
 import {TestTemplate} from './model/test-template';
 import {TestTemplateService} from '../services/test-template.service';
 import {NgbActiveModal, NgbModal, NgbModalRef} from '@ng-bootstrap/ng-bootstrap';
