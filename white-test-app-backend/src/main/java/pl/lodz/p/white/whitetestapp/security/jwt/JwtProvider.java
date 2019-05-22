bd51ab73d758b7a344b809235aa5a5024fb2edbf
Bartlomiej Ciach <203860@edu.p.lodz.pl>
2019-05-22 14:24:51 +0200

    MAS19WH-34 Optimize imports

--- white-test-app-backend/src/main/java/pl/lodz/p/white/whitetestapp/security/jwt/JwtProvider.java
+++ white-test-app-backend/src/main/java/pl/lodz/p/white/whitetestapp/security/jwt/JwtProvider.java
@@ -1,6 +1,11 @@
 package pl.lodz.p.white.whitetestapp.security.jwt;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
